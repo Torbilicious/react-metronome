44477e25e129e2997d7512cc7ef01b0147c98f71
Torbilicious <torben.binder@googlemail.com>
2017-09-27 01:00:55 +0200

    Fix test

--- src/Metronome.test.tsx
+++ src/Metronome.test.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './Metronome';
+import Metronome from './Metronome';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Metronome />, div);
 });
